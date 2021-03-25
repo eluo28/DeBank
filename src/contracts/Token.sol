e844f7ff7834917f947a85ccadaafc9467ba76a6
Edwin Luo <edwinluo28@gmail.com>
2021-03-25 14:22:33 -0400

    token contract

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -5,17 +5,27 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Token is ERC20 {
   //add minter variable
-
+  address public minter;
   //add minter changed event
+  event MinterChanged(address indexed from, address to);
 
-  constructor() public payable ERC20("Name", "Symbol") {
+  constructor() public payable ERC20("DeBank Coin", "DBC") {
     //asign initial minter
+    minter = msg.sender;
   }
 
   //Add pass minter role function
+    function passMinterRole(address dBank) public returns (bool) {
+  	require(msg.sender==minter, 'Error, only owner can change pass minter role');
+  	minter = dBank;
+
+    emit MinterChanged(msg.sender, dBank);
+    return true;
+  }
 
   function mint(address account, uint256 amount) public {
     //check if msg.sender have minter role
+    require(msg.sender == minter);
 		_mint(account, amount);
 	}
 }
