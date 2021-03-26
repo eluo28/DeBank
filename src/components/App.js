f9fa67f3a7235aa7a2cff103ea669fe7c9b65c99
Edwin Luo <edwinluo28@gmail.com>
2021-03-26 10:16:43 -0400

    front-end

--- src/components/App.js
+++ src/components/App.js
@@ -1,35 +1,62 @@
-import { Tabs, Tab } from 'react-bootstrap'
-import dBank from '../abis/dBank.json'
-import React, { Component } from 'react';
-import Token from '../abis/Token.json'
-import dbank from '../dbank.png';
-import Web3 from 'web3';
-import './App.css';
+import { Tabs, Tab } from "react-bootstrap";
+import dBank from "../artifacts/dBank.json";
+import React, { Component } from "react";
+import Token from "../artifacts/Token.json";
+import dbank from "../dbank.png";
+import Web3 from "web3";
+import "./App.css";
 
 //h0m3w0rk - add new tab to check accrued interest
 
 class App extends Component {
-
   async componentWillMount() {
-    await this.loadBlockchainData(this.props.dispatch)
+    
+    await this.loadBlockchainData(this.props.dispatch);
   }
 
   async loadBlockchainData(dispatch) {
+    if (typeof window.ethereum !== "undefined") {
+      const web3 = new Web3(window.ethereum);
+      const netId = await web3.eth.net.getId();
 
-    //check if MetaMask exists
+      const accounts = await web3.eth.getAccounts();
+      console.log(accounts);
+      //load balance
+      if (typeof accounts[0] !== "undefined") {
+        const balance = await web3.eth.getBalance(accounts[0]);
+        this.setState({ account: accounts[0], balance: balance, web3: web3 });
+      } else {
+        window.alert("Please login with MetaMask");
+      }
 
-      //assign to values to variables: web3, netId, accounts
-
-      //check if account is detected, then load balance&setStates, elsepush alert
-
-      //in try block load contracts
-
-    //if MetaMask not exists push alert
+      //load contracts
+      try {
+        const token = new web3.eth.Contract(
+          Token.abi,
+          Token.networks[netId].address
+        );
+        const dbank = new web3.eth.Contract(
+          dBank.abi,
+          dBank.networks[netId].address
+        );
+        const dBankAddress = dBank.networks[netId].address;
+        this.setState({
+          token: token,
+          dbank: dbank,
+          dBankAddress: dBankAddress,
+        });
+      } catch (e) {
+        console.log("Error", e);
+        window.alert("Contracts not deployed to the current network");
+      }
+    } else {
+      window.alert("Please install MetaMask");
+    }
   }
 
   async deposit(amount) {
     //check if this.state.dbank is ok
-      //in try block call dBank deposit();
+    //in try block call dBank deposit();
   }
 
   async withdraw(e) {
@@ -39,43 +66,35 @@ class App extends Component {
   }
 
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      web3: 'undefined',
-      account: '',
+      web3: "undefined",
+      account: "",
       token: null,
       dbank: null,
       balance: 0,
-      dBankAddress: null
-    }
+      dBankAddress: null,
+    };
   }
 
   render() {
     return (
-      <div className='text-monospace'>
+      <div className="text-monospace">
         <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-          <a
-            className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href="http://www.dappuniversity.com/bootcamp"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-        <img src={dbank} className="App-logo" alt="logo" height="32"/>
-          <b>dBank</b>
-        </a>
+          <div className="mt-2 mb-2 ml-3 text-white">DeBank</div>
         </nav>
         <div className="container-fluid mt-5 text-center">
-        <br></br>
+          <br></br>
           <h1>{/*add welcome msg*/}</h1>
           <h2>{/*add user address*/}</h2>
           <br></br>
           <div className="row">
             <main role="main" className="col-lg-12 d-flex text-center">
               <div className="content mr-auto ml-auto">
-              <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example">
-                {/*add Tab deposit*/}
-                {/*add Tab withdraw*/}
-              </Tabs>
+                <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example">
+                  {/*add Tab deposit*/}
+                  {/*add Tab withdraw*/}
+                </Tabs>
               </div>
             </main>
           </div>
