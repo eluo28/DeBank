6954d783c155c9550119d1537a827d16d9f6a5f0
Edwin Luo <edwinluo28@gmail.com>
2021-03-28 12:45:22 -0400

    Updates

---
+++ precache-manifest.0335ac4fef698b36372a9d93b8f3585e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "244e2e729732f3ee40dbce5261e514e4",
+    "url": "/DeBank/index.html"
+  },
+  {
+    "revision": "91736f2c40d860b0fba1",
+    "url": "/DeBank/static/css/2.963765e0.chunk.css"
+  },
+  {
+    "revision": "52e8ca318861c76beb71",
+    "url": "/DeBank/static/css/main.d850c1a7.chunk.css"
+  },
+  {
+    "revision": "91736f2c40d860b0fba1",
+    "url": "/DeBank/static/js/2.7ec2b853.chunk.js"
+  },
+  {
+    "revision": "f6ce9b198908e234c073cb4697347be2",
+    "url": "/DeBank/static/js/2.7ec2b853.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52e8ca318861c76beb71",
+    "url": "/DeBank/static/js/main.d7a8bc97.chunk.js"
+  },
+  {
+    "revision": "071c1f57bc80554f8c4a",
+    "url": "/DeBank/static/js/runtime-main.2d171a92.js"
+  }
+]);
